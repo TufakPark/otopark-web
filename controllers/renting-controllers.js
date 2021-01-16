dceaef9318ad3c6f8feeead2ce51b1b78d5d691d
Tobymeister <kerimdass@gmail.com>
2021-01-16 16:42:00 +0300

    getting park info with rent in allrentsbyuserid

--- controllers/renting-controllers.js
+++ controllers/renting-controllers.js
@@ -6,162 +6,172 @@ const Parking = require('../models/parking-model');
 const { validationResult } = require('express-validator');
 
 const getAllRentingController = async (req, res) => {
-  try {
-    const rents = await Renting.find();
-    if (!rents) {
-      return res.status(404).json({ message: 'Herhangi bir kiralama işlemi bulunamadı' });
+    try {
+        const rents = await Renting.find();
+        if (!rents) {
+            return res.status(404).json({ message: 'Herhangi bir kiralama işlemi bulunamadı' });
+        }
+        res.status(200).json({
+            rents: rents.map((rent) => rent.toObject())
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-    res.status(200).json({
-      rents: rents.map((rent) => rent.toObject())
-    });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 };
 
 const postRentingController = async (req, res) => {
-  const errors = validationResult(req);
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    const extractedErrors = []
-    errors.array().map(err => extractedErrors.push({ [err.param]: err.message }))
+    if (!errors.isEmpty()) {
+        const extractedErrors = []
+        errors.array().map(err => extractedErrors.push({ [err.param]: err.message }))
 
-    return res.status(422).json({ message: 'Geçersiz Girdi', errors: extractedErrors });
-  }
-
-  try {
-
-    const isExist = await Renting.findOne({
-      userid: req.body.userid,
-      parkid: req.body.parkid
-    });
-
-    if (isExist && isExist.used !== true) {
-      return res.status(422).json({ message: 'Kiralama işlemi zaten mevcut' });
+        return res.status(422).json({ message: 'Geçersiz Girdi', errors: extractedErrors });
     }
 
-    const park = await Parking.findById({ _id: req.body.parkid });
+    try {
 
-    if (park.carsnumber >= park.capacity) {
-      return res.status(500).json({ message: 'Otopark dolu' });
-    }
-
-    const newRent = new Renting(req.body);
-
-    await newRent.save();
-
-    Parking.findByIdAndUpdate(
-      { _id: req.body.parkid },
-      { $inc: { carsnumber: 1 } },
-      { new: true })
-      .exec()
-      .then((response) => {
-        res.status(200).json({
-          rent: newRent,
-          park: response
+        const isExist = await Renting.findOne({
+            userid: req.body.userid,
+            parkid: req.body.parkid
         });
-      });
 
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+        if (isExist && isExist.used !== true) {
+            return res.status(422).json({ message: 'Kiralama işlemi zaten mevcut' });
+        }
+
+        const park = await Parking.findById({ _id: req.body.parkid });
+
+        if (park.carsnumber >= park.capacity) {
+            return res.status(500).json({ message: 'Otopark dolu' });
+        }
+
+        const newRent = new Renting(req.body);
+
+        await newRent.save();
+
+        Parking.findByIdAndUpdate(
+            { _id: req.body.parkid },
+            { $inc: { carsnumber: 1 } },
+            { new: true })
+            .exec()
+            .then((response) => {
+                res.status(200).json({
+                    rent: newRent,
+                    park: response
+                });
+            });
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 };
 
 const postUsedRentByRentIDController = async (req, res) => {
-  try {
-    const id = req.body.id;
+    try {
+        const id = req.body.id;
 
-    await Renting.findByIdAndUpdate(
-      { _id: id },
-      {
-        used: true
-      },
-      { new: true })
-      .exec()
-      .then((response) => {
-        Parking.findByIdAndUpdate(
-          { _id: response.parkid },
-          { $inc: { carsnumber: -1 } },
-          { new: true })
-          .exec()
-          .then(() => {
-            res.status(200).json({
-              rent: response
+        await Renting.findByIdAndUpdate(
+            { _id: id },
+            {
+                used: true
+            },
+            { new: true })
+            .exec()
+            .then((response) => {
+                Parking.findByIdAndUpdate(
+                    { _id: response.parkid },
+                    { $inc: { carsnumber: -1 } },
+                    { new: true })
+                    .exec()
+                    .then(() => {
+                        res.status(200).json({
+                            rent: response
+                        });
+                    });
             });
-          });
-      });
 
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 };
 
 const getIDUsingUserParkIDController = async (req, res) => {
-  try {
-    const ids = req.body;
+    try {
+        const ids = req.body;
 
-    const exist = await Renting.find(ids);
-    if (exist) {
-      return res.status(200).json({ rents: exist.map((rent) => rent.toObject()) });
-    } else {
-      return res.status(404).json({ message: 'Bulunamadi' });
+        const exist = await Renting.find(ids);
+        if (exist) {
+            return res.status(200).json({ rents: exist.map((rent) => rent.toObject()) });
+        } else {
+            return res.status(404).json({ message: 'Bulunamadi' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 };
 
 const getRentInfoUsingRentIDController = async (req, res) => {
-  try {
-    const id = req.body;
+    try {
+        const id = req.body;
 
-    const exist = await Renting.findById(id);
-    if (exist) {
-      return res.status(200).json({ rent: exist });
-    } else {
-      return res.status(404).json({ message: 'Bulunamadi' });
+        const exist = await Renting.findById(id);
+        if (exist) {
+            return res.status(200).json({ rent: exist });
+        } else {
+            return res.status(404).json({ message: 'Bulunamadi' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 };
 
 const getAllRentsByUserIDController = async (req, res) => {
-  try {
+    try {
 
-    const exist = await Renting.find({ userid: req.body.userid });
-    if (exist) {
-      return res.status(200).json({ rents: exist.map((rent) => rent.toObject()) });
-    } else {
-      return res.status(404).json({ message: 'Bulunamadi' });
+        const exist = await Renting.find({ userid: req.body.userid });
+        if (exist) {
+            let rents = [];
+
+            for (const rent of exist) {
+                const park = await Parking.findById(rent.parkid).exec();
+                rents.push({
+                    rentinfo: rent,
+                    parkinfo: park
+                })
+            }
+
+            return res.status(200).json({ rents: rents });
+        } else {
+            return res.status(404).json({ message: 'Bulunamadi' });
+        }
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 };
 
 const getUsedInfoCOntroller = async (req, res) => {
-  try {
+    try {
 
-    const exist = await Renting.findOne({ _id: req.body._id });
-    if (exist.used) {
-      return res.status(200).json(true);
-    } else {
-      return res.status(404).json(false);
+        const exist = await Renting.findOne({ _id: req.body._id });
+        if (exist.used) {
+            return res.status(200).json(true);
+        } else {
+            return res.status(404).json(false);
+        }
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 };
 
 module.exports = {
-  getAllRentingController,
-  postRentingController,
-  getIDUsingUserParkIDController,
-  getRentInfoUsingRentIDController,
-  postUsedRentByRentIDController,
-  getAllRentsByUserIDController,
-  getUsedInfoCOntroller
+    getAllRentingController,
+    postRentingController,
+    getIDUsingUserParkIDController,
+    getRentInfoUsingRentIDController,
+    postUsedRentByRentIDController,
+    getAllRentsByUserIDController,
+    getUsedInfoCOntroller
 };
--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -8,239 +8,239 @@ const User = require('../models/users-model');
 const Parking = require('../models/parking-model');
 
 const getUsersController = async (req, res) => {
-  let users;
+    let users;
 
-  try {
-    users = await User.find({}, '-password');
-  } catch (err) {
-    return res.status(500).json({ message: 'Kullanıcı Bulunamadı' });
-  }
+    try {
+        users = await User.find({}, '-password');
+    } catch (err) {
+        return res.status(500).json({ message: 'Kullanıcı Bulunamadı' });
+    }
 
-  res.status(200).json({
-    users: users.map((user) => user.toObject())
-  });
+    res.status(200).json({
+        users: users.map((user) => user.toObject())
+    });
 };
 
 const signupController = async (req, res) => {
-  const errors = validationResult(req);
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    const extractedErrors = []
-    errors.array().map(err => extractedErrors.push({ [err.param]: err.message }))
+    if (!errors.isEmpty()) {
+        const extractedErrors = []
+        errors.array().map(err => extractedErrors.push({ [err.param]: err.message }))
 
-    return res.status(422).json({ message: 'Geçersiz Girdi', errors: extractedErrors });
-  }
-
-  const { email, password, passwordConfirmation } = req.body;
-
-  let isUserExist;
-
-  try {
-    isUserExist = await User.findOne({ email: email });
-  } catch (err) {
-    return res.status(500).json({ message: '<Kullanıcı mevcut mu?> sırasında hata meydana geldi' });
-  }
-
-  if (isUserExist) {
-    return res.status(422).json({ message: 'Kullanıcı zaten var' });
-  }
-
-  let hashedPassword;
-
-  try {
-    hashedPassword = await bcrypt.hash(password, 10);
-  } catch (err) {
-    return res.status(500).json({ message: '<Şifre oluşturma> sırasında hata meydana geldi' });
-  }
-
-  const newUser = new User({
-    email: email,
-    password: hashedPassword
-  });
-
-  try {
-    await newUser.save();
-  } catch (err) {
-    return res.status(500).json({ message: '<Kullanıcı kaydet> sırasında hata meydana geldi' });
-  }
-
-  let token;
-
-  try {
-    token = jwt.sign({
-      id: newUser.id
-    },
-      UP_JWT_SECRET,
-      {
-        expiresIn: 3600
-      });
-  } catch (err) {
-    return res.status(500).json({ message: '<Token oluşturma> sırasında hata meydana geldi' });
-  }
-
-  res.status(200).json({
-    token,
-    user: {
-      id: newUser.id,
-      email: newUser.email
+        return res.status(422).json({ message: 'Geçersiz Girdi', errors: extractedErrors });
     }
-  });
+
+    const { email, password, passwordConfirmation } = req.body;
+
+    let isUserExist;
+
+    try {
+        isUserExist = await User.findOne({ email: email });
+    } catch (err) {
+        return res.status(500).json({ message: '<Kullanıcı mevcut mu?> sırasında hata meydana geldi' });
+    }
+
+    if (isUserExist) {
+        return res.status(422).json({ message: 'Kullanıcı zaten var' });
+    }
+
+    let hashedPassword;
+
+    try {
+        hashedPassword = await bcrypt.hash(password, 10);
+    } catch (err) {
+        return res.status(500).json({ message: '<Şifre oluşturma> sırasında hata meydana geldi' });
+    }
+
+    const newUser = new User({
+        email: email,
+        password: hashedPassword
+    });
+
+    try {
+        await newUser.save();
+    } catch (err) {
+        return res.status(500).json({ message: '<Kullanıcı kaydet> sırasında hata meydana geldi' });
+    }
+
+    let token;
+
+    try {
+        token = jwt.sign({
+            id: newUser.id
+        },
+            UP_JWT_SECRET,
+            {
+                expiresIn: 3600
+            });
+    } catch (err) {
+        return res.status(500).json({ message: '<Token oluşturma> sırasında hata meydana geldi' });
+    }
+
+    res.status(200).json({
+        token,
+        user: {
+            id: newUser.id,
+            email: newUser.email
+        }
+    });
 };
 
 const loginController = async (req, res) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  let user;
+    let user;
 
-  try {
-    user = await User.findOne({ email: email });
-  } catch (err) {
-    return res.status(422).json({ message: '<Kullanıcı mevcut mu?> sırasında hata meydana geldi' });
-  }
-
-  if (!user) {
-    return res.status(401).json({ message: 'Mevcut kullanıcı bulunamadı. Girdilerinizi kontrol ediniz' });
-  }
-
-  let isPasswordValid = false;
-
-  try {
-    isPasswordValid = await bcrypt.compare(password, user.password);
-  } catch (err) {
-    return res.status(500).json({ message: '<Şifre karşılaştırma> sırasında hata meydana geldi' });
-  }
-
-  if (!isPasswordValid) {
-    return res.status(403).json({ message: 'Mevcut kullanıcı bulunamadı. Girdilerinizi kontrol ediniz' });
-  }
-
-  let token;
-
-  try {
-    token = jwt.sign({
-      id: user.id,
-      email: user.email
-    },
-      UP_JWT_SECRET,
-      {
-        expiresIn: 3600
-      })
-  } catch (err) {
-    return res.status(500).json({ message: '<Token oluşturma> sırasında hata meydana geldi' });
-  }
-
-  res.status(200).json({
-    token,
-    user: {
-      id: user.id,
-      email: user.email
+    try {
+        user = await User.findOne({ email: email });
+    } catch (err) {
+        return res.status(422).json({ message: '<Kullanıcı mevcut mu?> sırasında hata meydana geldi' });
     }
-  });
+
+    if (!user) {
+        return res.status(401).json({ message: 'Mevcut kullanıcı bulunamadı. Girdilerinizi kontrol ediniz' });
+    }
+
+    let isPasswordValid = false;
+
+    try {
+        isPasswordValid = await bcrypt.compare(password, user.password);
+    } catch (err) {
+        return res.status(500).json({ message: '<Şifre karşılaştırma> sırasında hata meydana geldi' });
+    }
+
+    if (!isPasswordValid) {
+        return res.status(403).json({ message: 'Mevcut kullanıcı bulunamadı. Girdilerinizi kontrol ediniz' });
+    }
+
+    let token;
+
+    try {
+        token = jwt.sign({
+            id: user.id,
+            email: user.email
+        },
+            UP_JWT_SECRET,
+            {
+                expiresIn: 3600
+            })
+    } catch (err) {
+        return res.status(500).json({ message: '<Token oluşturma> sırasında hata meydana geldi' });
+    }
+
+    res.status(200).json({
+        token,
+        user: {
+            id: user.id,
+            email: user.email
+        }
+    });
 };
 
 const tokenValidationController = async (req, res) => {
-  try {
-    const token = req.header("x-auth-token");
-    if (!token) return res.json(false);
+    try {
+        const token = req.header("x-auth-token");
+        if (!token) return res.json(false);
 
-    const verified = jwt.verify(token, UP_JWT_SECRET);
-    if (!verified) return res.json(false);
+        const verified = jwt.verify(token, UP_JWT_SECRET);
+        if (!verified) return res.json(false);
 
-    const user = await User.findById(verified.id);
-    if (!user) return res.json(false);
+        const user = await User.findById(verified.id);
+        if (!user) return res.json(false);
 
-    return res.status(200).json(true);
+        return res.status(200).json(true);
 
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 };
 
 const getOneUserController = async (req, res) => {
-  try {
-    const user = await User.findById(req.user);
-    return res.status(200).json({
-      id: user._id,
-      email: user.email,
-      name: user.name,
-      places: user.places,
-      registerdate: user.registerdate
-    })
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+    try {
+        const user = await User.findById(req.user);
+        return res.status(200).json({
+            id: user._id,
+            email: user.email,
+            name: user.name,
+            places: user.places,
+            registerdate: user.registerdate
+        })
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 };
 
 const updateUserController = async (req, res) => {
 
-  // TODO: check body if its empty
+    // TODO: check body if its empty
 
-  if (req.body.password !== req.body.passwordConfirmation) {
-    res.status(400).json({ message: 'Parolaları aynı giriniz' });
-  }
+    if (req.body.password !== req.body.passwordConfirmation) {
+        res.status(400).json({ message: 'Parolaları aynı giriniz' });
+    }
 
-  try {
-    req.body.password = await bcrypt.hash(req.body.password, 10);
-  } catch (err) {
-    return res.status(500).json({ message: '<Şifre oluşturma> sırasında hata meydana geldi' });
-  }
+    try {
+        req.body.password = await bcrypt.hash(req.body.password, 10);
+    } catch (err) {
+        return res.status(500).json({ message: '<Şifre oluşturma> sırasında hata meydana geldi' });
+    }
 
-  User.findByIdAndUpdate(req.user, req.body, { new: true })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).json({ message: 'Kullanıcı bulunamadı' });
-      }
+    User.findByIdAndUpdate(req.user, req.body, { new: true })
+        .then((user) => {
+            if (!user) {
+                return res.status(404).json({ message: 'Kullanıcı bulunamadı' });
+            }
 
-      return res.status(200).json({
-        user: {
-          id: user.id,
-          email: user.email,
-          name: user.name
-        }
-      });
-    })
-    .catch((err) => {
-      return res.status(500).json({ message: '<Kullanıcı güncelleştirme> sırasında hata meydana geldi' });
-    });
+            return res.status(200).json({
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    name: user.name
+                }
+            });
+        })
+        .catch((err) => {
+            return res.status(500).json({ message: '<Kullanıcı güncelleştirme> sırasında hata meydana geldi' });
+        });
 };
 
 const postFavouriteParkUserController = async (req, res) => {
-  await User.findByIdAndUpdate({ _id: req.user }, {
-    $push: { favourites: req.body.parkid }
-  }, { new: true })
-    .then(() => {
-      return res.status(200).json({ message: 'Favorilere eklendi' });
-    })
-    .catch((error) => {
-      return res.status(500).json({ message: error });
-    });
+    await User.findByIdAndUpdate({ _id: req.user }, {
+        $push: { favourites: req.body.parkid }
+    }, { new: true })
+        .then(() => {
+            return res.status(200).json({ message: 'Favorilere eklendi' });
+        })
+        .catch((error) => {
+            return res.status(500).json({ message: error });
+        });
 };
 
 const getAllFavouritesParkUserController = async (req, res) => {
-  const user = await User.findById({ _id: req.user }).then(async (user) => {
+    const user = await User.findById({ _id: req.user }).then(async (user) => {
 
-    let parks = [];
+        let parks = [];
 
-    for (const park of user.favourites) {
-      parks.push(await Parking.findOne({ _id: park }).exec());
-    }
+        for (const park of user.favourites) {
+            parks.push(await Parking.findOne({ _id: park }).exec());
+        }
 
-    res.status(200).json({
-      parks: parks.map((park => park.toObject()))
+        res.status(200).json({
+            parks: parks.map((park => park.toObject()))
+        })
     })
-  })
-    .catch((error) => {
-      return res.status(500).json({ message: error });
-    })
+        .catch((error) => {
+            return res.status(500).json({ message: error });
+        })
 };
 
 module.exports = {
-  getUsersController,
-  signupController,
-  loginController,
-  tokenValidationController,
-  getOneUserController,
-  updateUserController,
-  postFavouriteParkUserController,
-  getAllFavouritesParkUserController
+    getUsersController,
+    signupController,
+    loginController,
+    tokenValidationController,
+    getOneUserController,
+    updateUserController,
+    postFavouriteParkUserController,
+    getAllFavouritesParkUserController
 };
